716ba1919bea2f765f9531b3c4022c15b9431a0f
Kawan <kawanxd@hotmail.com>
2024-10-24 14:38:35 -0300

    subindo primeiro repositorio

---
+++ atividade-react/src/App.tsx
@@ -0,0 +1,13 @@
+// App.js
+
+import React from "react"; 
+import Logo from "./components/Logo"; 
+import Navegacao from "./components/Navegacao";
+import Principal from "./components/Principal";
+
+const App = () => { 
+ return <Logo />; 
+ return <Principal/>;
+}; 
+
+export default App
